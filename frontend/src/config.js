f0152174d5b01764f94e6821227c0a6a9fbd4f7f
Chirag Mandal <chiragmndl@gmail.com>
2023-06-26 18:40:12 +0530

    change

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,9 @@
 //Configure this acchording to the server listning port
 
-const HrBaseURL='http://localhost:5500';
-const EmpBaseUrl='http://localhost:5500';
+// const HrBaseURL='http://ec2-13-49-67-51.eu-north-1.compute.amazonaws.com:5500';
+// const EmpBaseUrl='http://ec2-13-49-67-51.eu-north-1.compute.amazonaws.com:5500';
 
+ const HrBaseURL='http://localhost:5500';
+ const EmpBaseUrl='http://localhost:5500';
 
 module.exports={HrBaseURL,EmpBaseUrl};
